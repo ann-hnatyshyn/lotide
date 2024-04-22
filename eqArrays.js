5febaef2c3a9487322cd17dbba947249b3abbe9f
Ann <a.m.hnatyshyn@gmail.com>
2024-04-22 00:58:06 -0300

    final

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,17 @@
-//const assertEqual = function(actual, expected) {
-//  if (actual === expected) {
-//   console.log("✅Assertion Passed:", `${actual}`, "===", `${expected}`);
-//  } else {
-//    console.log("❌Assertion Failed:", `${actual}`, "!==", `${expected}`);
-//  }
-//}; assertEqual(3, 4);
-
-const eqArrays = function (arr1, arr2) {
-  if (arr1 === arr2) {
-    return true;
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+   console.log("✅Assertion Passed:", `${actual}`, "===", `${expected}`);
   } else {
-    return false;
-  }
+    console.log("❌Assertion Failed:", `${actual}`, "!==", `${expected}`);
+ }
 };
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++)
+    if (arr1.length === arr1.length && arr1[i] === arr2[i]) {
+      return true;
+    } else {
+      false;
+    }
+};
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
