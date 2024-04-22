801fa43541af9e085bd1e13af98665cb9c2175aa
Ann <a.m.hnatyshyn@gmail.com>
2024-04-22 02:50:39 -0300

    final

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -17,8 +17,13 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-const assertArraysEqual = function(eqArrays) {
-  if true; {
-  return ("✅Assertion Passed" || "❌Assertion Failed");
-}
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
 };
+
+assertArraysEqual([1,2,3], [1,2,3]);
+assertArraysEqual([1,2,3], [1,2]);
