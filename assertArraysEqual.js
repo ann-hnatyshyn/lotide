d681f46f146ed590c2338b60b2542a2efd07f31a
Ann <a.m.hnatyshyn@gmail.com>
2024-04-22 01:52:49 -0300

    3rd attempt

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -7,9 +7,14 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  arr1.length === arr2.length &&
-  arr1.every((element, index) => element === arr2[index]);
+  if (arr1.length === arr2.length &&
+    arr1.every((element, index) => element === arr2[index])) {
+    return true;
+  } else {
+    return false;
+  }
 };
+console.log(eqArrays[3,2,1], [7,22,9]);
 
 const assertArraysEqual = function(arr1, arr2) {
 
