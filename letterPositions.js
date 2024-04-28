de4ce0bad67f4f6e226884714e3082e3830bc203
Ann <a.m.hnatyshyn@gmail.com>
2024-04-28 00:20:50 -0300

    first small string test

---
+++ letterPositions.js
@@ -0,0 +1,32 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  return results;
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++)
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  return true;
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++)
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  return true;
+};
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+
+const string2 = "hello";
+
+console.log();
