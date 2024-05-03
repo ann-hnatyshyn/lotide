29a8d44a5062ea78d9d6b760e4003ab75969354d
Ann <a.m.hnatyshyn@gmail.com>
2024-05-03 14:21:23 -0300

    complete, worked with mentor Olawale A, fixed console.log

--- takeUntil.js
+++ takeUntil.js
@@ -33,6 +33,9 @@ const numberLessThan6 = (x) => {
 };
 
 const arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr2 = [0, 1, 2, 3, 4, 5];
 
 
 console.log(takeUntil(arr1, numberLessThan6));
+
+console.log(assertArraysEqual(takeUntil(arr1, numberLessThan6), arr2));
