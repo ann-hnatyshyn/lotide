85dd3136ee21affb1182575cf9c5ccde53af118e
Ann <a.m.hnatyshyn@gmail.com>
2024-05-03 14:18:49 -0300

    complete, worked with mentor Olawale A

--- takeUntil.js
+++ takeUntil.js
@@ -33,8 +33,6 @@ const numberLessThan6 = (x) => {
 };
 
 const arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const arr2 = [0, 1, 2, 3, 4, 5];
+
 
 console.log(takeUntil(arr1, numberLessThan6));
-
-console.log(eqArrays(arr1, arr2));
