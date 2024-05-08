91d0efc58615829531fde75debb13225646e2fb6
Ann <a.m.hnatyshyn@gmail.com>
2024-05-08 20:12:40 -0300

    complete, headTest file

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,5 @@
 const head = require("../head.js");
 const assertEqual = require("../assertEqual");
 
-assertEqual(head([5, 6, 7], [1, 2]));
+assertEqual(head([5, 6, 7]),5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
