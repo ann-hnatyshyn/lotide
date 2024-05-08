2fb0e80c18f194adcfbe969df6fba3c956a35b00
Ann <a.m.hnatyshyn@gmail.com>
2024-05-08 20:58:39 -0300

    complete, tail function

--- tail.js
+++ tail.js
@@ -1,23 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅Assertion Passed:", `${actual}`, "===", `${expected}`);
-  } else {
-    console.log("❌Assertion Failed:", `${actual}`, "!==", `${expected}`);
-  }
-};
-
-
 const tail = function(arr) {
   return arr.length && arr.slice(1);
 };
-
-//correct answer:
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-//test case:
-const words = ["yo yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
