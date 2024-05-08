96e9f06f0ebfe00aa986315c84c86b7a9564c116
Ann <a.m.hnatyshyn@gmail.com>
2024-05-08 19:57:15 -0300

    complete, assertEqualTest file

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,5 @@
 const assertEqual = require("../assertEqual");
 
-assertEqual([5, 6, 7]), 5;
-assertEqual(["Hello", "Lighthouse", "Labs"]), "Goodbye";
+assertEqual("hello", "hello");
+assertEqual("The sky is blue", "The sky is green");
 
