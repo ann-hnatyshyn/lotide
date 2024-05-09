d544eeb6a5fa64c324962191e75370b9569d3fd7
Ann <a.m.hnatyshyn@gmail.com>
2024-05-08 21:19:35 -0300

    complete, eqArrays function

---
+++ test/eqArraysTest.js
@@ -0,0 +1,5 @@
+const eqArrays = require("../eqArrays.js");
+const assertEqual = require("../assertEqual");
+
+assertEqual(eqArrays([0, 9, 8, 7], [6, 5, 4, 3]), false);
+assertEqual(eqArrays([1, 2, 3, 4], [5, 6, 7, 8]), false);
--- test/tailTest.js
+++ test/tailTest.js
@@ -10,3 +10,6 @@ assertEqual(result[1], "Labs");
 const words = ["yo yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);
+
+assertEqual(tail(["test"]).length, 0); // tail of array with one element only should be empty
+assertEqual(tail([]).length, 0); // tail of an empty array should also be empty
