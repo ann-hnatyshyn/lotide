dd1773479989b57a1fc464f1d5d68b76b186807f
Ann <a.m.hnatyshyn@gmail.com>
2024-05-08 21:21:02 -0300

    complete, eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅Assertion Passed:", `${actual}`, "===", `${expected}`);
-  } else {
-    console.log("❌Assertion Failed:", `${actual}`, "!==", `${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -16,4 +8,5 @@ const eqArrays = function(arr1, arr2) {
     }
   return true;
 };
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+
+module.exports = eqArrays;
