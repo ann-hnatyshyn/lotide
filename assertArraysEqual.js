8193791d46579bf4e36a995a94f9f224e7a61ee1
Ann <a.m.hnatyshyn@gmail.com>
2024-05-08 21:52:52 -0300

    complete, assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅Assertion Passed:", `${actual}`, "===", `${expected}`);
-  } else {
-    console.log("❌Assertion Failed:", `${actual}`, "!==", `${expected}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
@@ -16,14 +8,4 @@ const eqArrays = function(arr1, arr2) {
     }
   return true;
 };
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-assertArraysEqual([1,2,3], [1,2,3]);
-assertArraysEqual([1,2,3], [1,2]);
+module.exports = assertArraysEqual;
