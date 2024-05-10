9278e9264ff6fad4db432e7d888d23740993b5f3
Ann <a.m.hnatyshyn@gmail.com>
2024-05-10 18:34:10 -0300

    complete, middleTest file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,8 @@
 const middle = require("../middle.js");
-const assertArraysEqual = require("../assertArraysEqual.js");
+const assertArraysEqual = require("../assertArraysEqual");
 
 console.log(middle([1, 2, 3, 4, 5]));
 
-assertArraysEqual(middle([1, 2, 3, 4]));
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]));
-assertArraysEqual(middle([1, 2, 3, 4, 5]));
+assertArraysEqual([1, 2, 3, 4], [4,3,2,1]);
+assertArraysEqual([1, 2, 3, 4, 5, 6], [5,6,7,8]);
+assertArraysEqual([1, 2, 3, 4, 5], [1,2,3,4]);
