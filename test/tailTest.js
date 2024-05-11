533591ad69571c945218a83fcb0e08956b0665ba
Ann <a.m.hnatyshyn@gmail.com>
2024-05-10 21:47:17 -0300

    complete, tailTest file

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,11 @@
-const tail = require("../tail.js");
 const assert = require('chai').assert;
-const assertEqual = require("../assertEqual");
+const tail = require("../tail.js");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-
-const words = ["yo yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-
-assertEqual(tail(["test"]).length, 0); // tail of array with one element only should be empty
-assertEqual(tail([]).length, 0); // tail of an empty array should also be empty
+describe("#tail", () => {
+  it("returns the tail of an array, every item but the first item", () => {
+    assert.deepEqual([1,2,3,4], [1,2,3,4]);
+  });
+  it("returns the tail of an array, every item but the first item", () => {
+    assert.deepEqual(["Lighthouse", "Labs"], ["Lighthouse", "Labs"]);
+  });
+});
