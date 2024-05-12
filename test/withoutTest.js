45fc18641e4121dcd0d4abbf97ac2f1ac2246a04
Ann <a.m.hnatyshyn@gmail.com>
2024-05-11 23:02:51 -0300

    fix without function

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -6,5 +6,5 @@ assertArraysEqual(without([1,2,3], [1,2,3]));
 assertArraysEqual(without([1,2,3], [1,2]));
 
 const words = ["hello", "world"];
-without(words, ["lighthouse"]);
+console.log(without(words, ["lighthouse"]));
 assertArraysEqual(without(words["hello", "world", "lighthouse"]));
